fed65c6e6dbb7387d91423802172ecc202dc5654
Rahul <amaleexxii@gmail.com>
2025-01-25 17:31:29 +0530

    Refactor Home component: improve code formatting and structure

--- frontend/src/pages/client/Home.tsx
+++ frontend/src/pages/client/Home.tsx
@@ -5,19 +5,16 @@ import car_anim from "../../assets/car-animation.json";
 const Home: React.FC = () => {
   return (
     <div className="bg-black min-h-screen flex flex-col">
-    {/* Navbar */}
-    <header className="z-10">
-      <Navbar />
-    </header>
-
-    {/* Lottie Animation */}
-    <main className="flex-1 flex justify-center items-center">
-      <Lottie
-        animationData={car_anim}
-        className="w-full h-screen bg-black overflow-hidden"
-      />
-    </main>
-  </div>
+      <header className="z-10">
+        <Navbar />
+      </header>
+      <main className="flex-1 flex justify-center items-center">
+        <Lottie
+          animationData={car_anim}
+          className="w-full h-screen bg-black overflow-hidden"
+        />
+      </main>
+    </div>
   );
 };
 
