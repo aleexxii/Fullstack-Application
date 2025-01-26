c5982cfef180c5b426157b64d45e4a4ad471ad38
Rahul <amaleexxii@gmail.com>
2025-01-26 18:12:05 +0530

    Add User model with schema, password hashing, and comparison method

---
+++ backend/src/model/User.ts
@@ -0,0 +1,52 @@
+import mongoose, { Document, Schema } from "mongoose";
+import bcryptJs from "bcryptjs";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: "admin" | "user";
+  profilePicture : string;
+  comparePassword: (enteredPassword: string) => Promise<boolean>;
+}
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role : {
+    type : String,
+    enum : ['admin', 'user'],
+    default : 'user'
+  },
+  profilePicture : {
+    type : String,
+    default : "https://th.bing.com/th/id/OIP.SAcV4rjQCseubnk32USHigHaHx?rs=1&pid=ImgDetMain"
+  }
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcryptJs.genSalt(10);
+  this.password = await bcryptJs.hash(this.password, salt);
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword: string) {
+  return await bcryptJs.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
