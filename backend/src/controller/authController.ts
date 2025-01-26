8db7673c917ebcae8ae3ed05f3699914a3ac301d
Rahul <amaleexxii@gmail.com>
2025-01-26 20:44:18 +0530

    Add MongoDB connection, JWT token generation, and user authentication logic

--- backend/src/controller/authController.ts
+++ backend/src/controller/authController.ts
@@ -1,14 +1,53 @@
 import { Request, Response } from "express";
+import User from "../model/User";
+import { generateToken } from "../utils/generateToken";
 
-const registerUser = async (req: Request, res: Response) => {
+
+const registerUser = async (req: Request, res: Response) : Promise<Response> => {
+  const { name, email, password } = req.body;
   try {
-    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if(existingUser){
+        return res.status(400).json({message : 'User is already exists'})
+    }
+
+    const user = new User({
+        name,
+        email,
+        password
+    })
+    await user.save()
+    return res.status(200).json({message : 'User registered successfully'})
   } catch (error) {
-    console.log(error);
+    return res.status(500).json({message : 'Internal server error'})
   }
 };
 
-const login = async (req: Request, res: Response) => {};
+const login = async (req: Request, res: Response) : Promise<Response> => {
+    const {email, password} = req.body
+    try {
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.status(400).json({message : 'User not found'})
+        }
+
+        const passwordMatch = await user?.comparePassword(password)
+        console.log('passwordMatch :' , passwordMatch);
+
+        if(!passwordMatch){
+            return res.status(400).json({message : 'Incorrect password'})
+        }
+        
+        const token = generateToken(user)
+
+        return res.status(200).json({token})
+
+    } catch (error) {
+        return res.status(500).json({message : 'Internal server error'})
+    }
+};
 
 const logout = (req: Request, res: Response) => {};
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,14 @@ import express from 'express'
 import authRouter from './routes/authRouter'
 import dotenv from 'dotenv'
 dotenv.config()
-
+import ConnectDB from './config/connectDB'
 const app = express()
-const PORT = process.env.PORT || 4000
 
-app.use(authRouter)
+app.use(express.json())
+
+const PORT = process.env.PORT || 4000
+app.use('/auth',authRouter)
+ConnectDB()
 
 app.listen(PORT, () =>{
     console.log(`server running on port : ${PORT}`);
