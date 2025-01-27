86755c9cda5eb8fbb5d933093625f87b6d3b63be
Rahul <amaleexxii@gmail.com>
2025-01-27 11:52:34 +0530

    Update generateToken function to set JWT as an HTTP-only cookie, updated logout functionality

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -1,6 +1,15 @@
-import jwt from 'jsonwebtoken'
-import { IUser } from '../model/User'
+import { Response } from "express";
+import jwt from "jsonwebtoken";
+import { IUser } from "../model/User";
 
-export const generateToken = (user : IUser) =>{
-    return jwt.sign({id : user._id, role : user.role}, process.env.JWT_SECRET!, {expiresIn : '1d'})
-}
+export const generateToken = (res: Response, user: IUser) => {
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET!,
+    { expiresIn: "1d" }
+  );
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    maxAge: 60 * 60 * 1000,
+  });
+};
