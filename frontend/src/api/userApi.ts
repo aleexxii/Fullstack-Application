2fae30d1571874d94e8f3770120665e149bb535f
Rahul <amaleexxii@gmail.com>
2025-02-12 19:02:22 +0530

    Add user API module: implement fetchUserInfo and updateUserInfo functions for user data retrieval and updates

---
+++ frontend/src/api/userApi.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { API_URL } from "./authApi";
+
+const api = axios.create({
+    baseURL: API_URL,
+    withCredentials: true,
+})
+
+export const fetchUserInfo = async (userId : string) => {
+    return api.get(`/user/${userId}`);
+}
+
+export const updateUserInfo = async (userId : string, userData : {username : string, email : string}) => {
+    return api.put(`/user/${userId}`, userData);
+}
